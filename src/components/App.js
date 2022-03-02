0a47a035719b4d3ccd12f2f4a44eb707c312bb3d
MaggieTheCoder <mdurandt@gmail.com>
2022-03-02 16:05:35 +0200

    completed project setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Hauze</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
