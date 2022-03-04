25eaa3aeff139a2ee178ddeff678d645cd35947a
Heli Devine <heli.devine@gmail.com>
2022-03-04 18:37:52 +0000

    working with addtask component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../styles/App.css";
+import AddTask from "./AddTask";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <h2>Hauze</h2>
+        <AddTask />
       </header>
     </div>
   );
