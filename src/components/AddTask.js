3811bf3689b7ce6adb63fe18f244eab2addb1e96
Heli Devine <heli.devine@gmail.com>
2022-03-08 19:30:06 +0000

    add task component debug

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,35 +3,57 @@ import "../styles/AddTask.css";
 
 const AddTask = () => {
   const [fields, setFields] = useState();
+  const [custom, setCustom] = useState(false);
 
   const handleAddTask = (event) => {
     event.preventDefault();
-    console.log(fields);
+    // console.log("****", fields);
   };
   const handleFieldChange = (event) => {
+    console.log("**", event.target.value);
+    if (event.target.value === "custom") {
+      setCustom(true);
+    } else {
+      setCustom(false);
+      setFields(event.target.value);
+    }
+  };
+  const handleInput = (event) => {
+    // console.log(event.target.value, fields);
     setFields(event.target.value);
   };
 
   return (
-    <div className="add-task">
-      <form className="task__list" onSubmit={handleAddTask}>
-        <label htmlFor="type">
-          Type
-          <select
-            id="type"
-            name="type"
-            value={fields}
-            onChange={handleFieldChange}
-          >
-            <option value="Empty Dishwasher">Empty Dishwasher</option>
-            <option value="Hoover living room">Hoover living room</option>
-            <option value="Water the plants">Water the plants</option>
-          </select>
-        </label>
+    <>
+      <div className="add-task">
+        <form className="task__list" onSubmit={handleAddTask}>
+          <label htmlFor="type">
+            Type
+            <select
+              id="type"
+              name="type"
+              value={fields}
+              onChange={handleFieldChange}
+            >
+              <option value="Empty Dishwasher">Empty Dishwasher</option>
+              <option value="Hoover living room">Hoover living room</option>
+              <option value="Water the plants">Water the plants</option>
+              <option value="custom">Add your own</option>
+            </select>
+          </label>
 
-        <button type="submit">Add Task</button>
-      </form>
-    </div>
+          {!custom && <button type="submit">Add Task</button>}
+        </form>
+      </div>
+      {custom && (
+        <div className="custom-task">
+          <form onSubmit={handleAddTask}>
+            <input type="text" onChange={handleInput} placeholder="add task" />
+            <button type="submit">Add Task</button>
+          </form>
+        </div>
+      )}
+    </>
   );
 };
 
